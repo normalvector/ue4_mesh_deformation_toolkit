5203278ef4706123ec9f04026de1061c65d151e6
U-FRANCESCA\molt <Paul.Golds+normalvector@GMail.com>
2017-07-27 20:22:58 +0100

    Created a Utility class which can contain the utility methods.

---
+++ MeshDeformationTK/Plugins/MeshDeformationToolkit/Source/MeshDeformationToolkit/Private/Utility.cpp
@@ -0,0 +1,5 @@
+// (c)2017 Paul Golds, released under MIT License.
+
+#include "MeshDeformationToolkit.h"
+#include "Utility.h"
+
---
+++ MeshDeformationTK/Plugins/MeshDeformationToolkit/Source/MeshDeformationToolkit/Public/Utility.h
@@ -0,0 +1,7 @@
+// (c)2017 Paul Golds, released under MIT License.
+
+#pragma once
+
+#include "UObject/NoExportTypes.h"
+// #include "Utility.generated.h"
+
