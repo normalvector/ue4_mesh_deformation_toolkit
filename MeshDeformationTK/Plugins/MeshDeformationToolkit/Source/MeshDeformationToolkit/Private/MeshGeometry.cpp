7a65f3a9ebb824dc37d7a483eced359568f09a1b
Paul Golds <Paul.Golds+normalvector@GMail.com>
2017-11-14 15:43:06 +0000

    Started work on a Lerp demo- but seems I need better meshes to test with.

--- MeshDeformationTK/Plugins/MeshDeformationToolkit/Source/MeshDeformationToolkit/Private/MeshGeometry.cpp
+++ MeshDeformationTK/Plugins/MeshDeformationToolkit/Source/MeshDeformationToolkit/Private/MeshGeometry.cpp
@@ -608,10 +608,17 @@ void UMeshGeometry::Lerp(UMeshGeometry *TargetMeshGeometry, float Alpha /*= 0.0f
 			// Get the existing data from the two components.
 			FVector VertexFromThis = this->Sections[SectionIndex].Vertices[VertexIndex];
 			FVector VertexFromTarget = TargetMeshGeometry->Sections[SectionIndex].Vertices[VertexIndex];
+			FVector NormalFromThis = this->Sections[SectionIndex].Normals[VertexIndex];
+			FVector NormalFromTarget = TargetMeshGeometry->Sections[SectionIndex].Normals[VertexIndex];
 
 			// TODO: World/local logic should live here.
 			this->Sections[SectionIndex].Vertices[VertexIndex] = FMath::Lerp(
 				VertexFromThis, VertexFromTarget,
+				Alpha * (Selection ? Selection->Weights[NextWeightIndex++] : 1.0f)
+			);
+			// TODO: World/local logic should live here.
+			this->Sections[SectionIndex].Normals[VertexIndex] = FMath::Lerp(
+				NormalFromThis, NormalFromTarget,
 				Alpha * (Selection ? Selection->Weights[NextWeightIndex++] : 1.0f)
 			);
 		}
